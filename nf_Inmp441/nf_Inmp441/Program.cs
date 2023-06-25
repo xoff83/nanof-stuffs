a669fa6b0e0e88394d969f3828b17289e7e081dc
unknown <christophe.panizza@gmail.com>
2023-06-25 16:29:18 +0200

    montée de version

--- nf_Inmp441/nf_Inmp441/Program.cs
+++ nf_Inmp441/nf_Inmp441/Program.cs
@@ -2,7 +2,7 @@ using nanoFramework.Hardware.Esp32;
 using nf_Utils;
 using System;
 using System.Device.Adc;
-// waiting for : using System.Device.I2S;
+using System.Device.I2s;
 using System.Diagnostics;
 using System.Threading;
 
@@ -11,38 +11,38 @@ namespace nf_Inmp441
     public class Program
     {
         /**
-        * Téléchargement de données: https://pan.baidu.com/s/1bQ5_QeCSIL2UOH7wSF_IFg
+        * TÃ©lÃ©chargement de donnÃ©es: https://pan.baidu.com/s/1bQ5_QeCSIL2UOH7wSF_IFg
         * 
         * Lancement de produit:
-        * INMP441 est un microphone MEMS omnidirectionnel à haute performance, faible consommation d'énergie, sortie numérique avec port inférieur.
-        * La solution complète INMP441 se compose d'un capteur MEMS, d'un conditionnement de la composition du signal, d'un convertisseur analogique-numérique, 
-        * d'un filtre anti-crénelage, d'une gestion de l'alimentation et d'une interface i² 24 bits standard de l'industrie. L'interface i²s permet à INMP441 
-        * de se connecter directement aux processeurs numériques, tels que DSPs et microcontrôleurs, sans avoir besoin du codec audio utilisé dans le système. 
-        * INMP441 a un rapport signal/bruit élevé et est un excellent choix pour les applications en champ proche. 
-        * INMP441 a une réponse en fréquence à large bande plate, ce qui entraîne une haute définition du son naturel.
+        * INMP441 est un microphone MEMS omnidirectionnel Ã  haute performance, faible consommation d'Ã©nergie, sortie numÃ©rique avec port infÃ©rieur.
+        * La solution complÃ©te INMP441 se compose d'un capteur MEMS, d'un conditionnement de la composition du signal, d'un convertisseur analogique-numÃ©rique, 
+        * d'un filtre anti-crÃ©nelage, d'une gestion de l'alimentation et d'une interface iÃ© 24 bits standard de l'industrie. L'interface iÂ²s permet Ã  INMP441 
+        * de se connecter directement aux processeurs numÃ©riques, tels que DSPs et microcontrÃ©leurs, sans avoir besoin du codec audio utilisÃ© dans le systÃ©me. 
+        * INMP441 a un rapport signal/bruit Ã©levÃ© et est un excellent choix pour les applications en champ proche. 
+        * INMP441 a une rÃ©ponse en frÃ©quence Ã  large bande plate, ce qui entraÃ©ne une haute dÃ©finition du son naturel.
         * 
-        * Caractéristiques du produit:
-        * 1. interface i²s numérique avec données 24 bits de haute précision
-        * 2. Rapport signal/bruit élevé de 61 dBA
-        * 3. Haute sensibilité -26 dBFS
-        * 4. Réponse en fréquence Stable de 60Hz à 15 kHz
-        * 5. Faible consommation d'énergie: faible consommation de courant de 1.4 mA
-        * 6. PSR élevé: -75 dBFS
+        * CaractÃ©ristiques du produit:
+        * 1. interface iÂ²s numÃ©rique avec donnÃ©es 24 bits de haute prÃ©cision
+        * 2. Rapport signal/bruit Ã©levÃ© de 61 dBA
+        * 3. Haute sensibilitÃ© -26 dBFS
+        * 4. RÃ©ponse en frÃ©quence Stable de 60Hz Ã  15 kHz
+        * 5. Faible consommation d'Ã©nergie: faible consommation de courant de 1.4 mA
+        * 6. PSR Ã©levÃ©: -75 dBFS
         * 
-        * Définition de l'interface:
-        *   SCK:        horloge de données série de l'interface i²s
-        *   WS:         sélection du mot de données série pour l'interface i²s
-        *   L/R:        sélection de canal gauche/droite.
-        *                   Lorsqu'il est réglé au niveau bas, le microphone émet des signaux sur le canal gauche du cadre i²s.
-        *                   Lorsqu'il est réglé à un niveau élevé, le microphone émet le signal sur le canal droit 
-        *   SD:         sortie de données série de l'interface i²s.
-        *   VCC:        puissance d'entrée, 1.8V à 3.3V.
+        * DÃ©finition de l'interface:
+        *   SCK:        horloge de donnÃ©es sÃ©rie de l'interface iÂ²s
+        *   WS:         sÃ©lection du mot de donnÃ©es sÃ©rie pour l'interface iÂ²s
+        *   L/R:        sÃ©lection de canal gauche/droite.
+        *                   Lorsqu'il est rÃ©glÃ© au niveau bas, le microphone Ã©met des signaux sur le canal gauche du cadre iÂ²s.
+        *                   Lorsqu'il est rÃ©glÃ© Ã  un niveau Ã©levÃ©, le microphone Ã©met le signal sur le canal droit 
+        *   SD:         sortie de donnÃ©es sÃ©rie de l'interface iÂ²s.
+        *   VCC:        puissance d'entrÃ©e, 1.8V Ã  3.3V.
         *   LA TERRE:   puissance sol
         *   
         * Ce produit fournit des tutoriels pour l'utilisation du module ESP32 avec fonction I2S
         * 
-        * Connectez-vous à notre carte de développement ESP32:
-        * INMP441   ESP32 Carte de Développement
+        * Connectez-vous Ã  notre carte de dÃ©veloppement ESP32:
+        * INMP441   ESP32 Carte de DÃ©veloppement
         * LA SCK    >> GPIO14 : ADC2 CH6
         * SD        >> GPIO32 : ADC1 CH4
         * WS        >> GPIO15 : ADC1 CH3
@@ -124,6 +124,70 @@ namespace nf_Inmp441
              *     I2S is similar to SPI
              */
 
+
+
+
+            //     I2S1/2 function Master Clock. Used only in master mode.
+            //Configuration.SetPinFunction(Gpio.IO32, DeviceFunction.I2S1_MCK);
+
+            //     I2S1/2 function Bit Clock. Used for general purpose read and write on the I2S bus.
+            Configuration.SetPinFunction(Gpio.IO33, DeviceFunction.I2S1_BCK);
+
+            //     I2S1/2 function WS. Used if your have stereo.
+            Configuration.SetPinFunction(Gpio.IO34, DeviceFunction.I2S1_WS);
+
+            //     I2S1/2 function DATA_OUT. Used for output data typically on a speaker.
+            //Configuration.SetPinFunction(Gpio.IO32, DeviceFunction.I2S1_DATA_OUT);
+
+            //     I2S1/2 function MDATA_IN. Used for input data typically from a microphone.
+            Configuration.SetPinFunction(Gpio.IO32, DeviceFunction.I2S1_MDATA_IN);
+
+
+            I2sDevice i2s = new(new I2sConnectionSettings(1) {
+                BitsPerSample = I2sBitsPerSample.Bit8,
+                ChannelFormat = I2sChannelFormat.OnlyLeft,
+                Mode = I2sMode.Master | I2sMode.Rx | I2sMode.Pdm,
+                CommunicationFormat = I2sCommunicationFormat.I2S,
+                SampleRate = 8_000
+            });
+
+            // should be one second of sound data:
+            SpanByte buff = new byte[8000];
+            i2s.Read(buff);
+            i2s.Dispose();
+            int valSd = 0;
+
+            while (true)
+            {
+
+                try
+                {
+                    //// Get the value
+
+                    valSd = i2s.ReadValue();
+
+                    //Console.WriteLine($"SCK: {valSck} , WS: {valWs} , SD: {valSd}");
+                    Console.WriteLine($"SCK: {acSckViolet.ReadRatio()}% , WS: {acSckViolet.ReadRatio()}% , SD: {acSdGris.ReadRatio()}");
+
+
+
+                }
+                catch (Exception ex)
+                {
+                    // Do whatever pleases you with the exception caught
+                    Console.WriteLine(ex.ToString());
+                }
+                // Very slow sampling rate 
+                Thread.Sleep(250);
+            }
+
+
+
+        }
+
+
+        public static void adcMain(){
+
             //https://github.com/ikostoski/esp32-i2s-slm
             //https://fr.wikipedia.org/wiki/I2S
             // tuto ADC PWM : https://www.upesy.fr/blogs/tutorials/measure-voltage-on-esp32-with-adc-with-arduino-code
@@ -152,34 +216,15 @@ namespace nf_Inmp441
              *             GPIO: 32     |       GPIO: 3V3       |          GPIO:  GND
              *              
              *              
-             *   SCK:        horloge de données série de l'interface i²s
-             *   WS:         sélection du mot de données série pour l'interface i²s
-             *   L/R:        sélection de canal gauche/droite.
-             *                   Lorsqu'il est réglé au niveau bas, le microphone émet des signaux sur le canal gauche du cadre i²s.
-             *                   Lorsqu'il est réglé à un niveau élevé, le microphone émet le signal sur le bon canal 
-             *   SD:         sortie de données série de l'interface i²s.
-             *   VCC:        puissance d'entrée, 1.8V à 3.3V.
+             *   SCK:        horloge de donnÃ©es sÃ©rie de l'interface iÂ²s
+             *   WS:         sÃ©lection du mot de donnÃ©es sÃ©rie pour l'interface iÂ²s
+             *   L/R:        sÃ©lection de canal gauche/droite.
+             *                   Lorsqu'il est rÃ©glÃ© au niveau bas, le microphone Ã©met des signaux sur le canal gauche du cadre iÂ²s.
+             *                   Lorsqu'il est rÃ©glÃ© Ã  un niveau Ã©levÃ©, le microphone Ã©met le signal sur le bon canal 
+             *   SD:         sortie de donnÃ©es sÃ©rie de l'interface iÂ²s.
+             *   VCC:        puissance d'entrÃ©e, 1.8V Ã  3.3V.
              *   GND:        puissance sol              
              */
-
-
-            //     I2S1/2 function Master Clock. Used only in master mode.
-            //Configuration.SetPinFunction(Gpio.IO32, DeviceFunction.I2S1_MCK);
-
-            //     I2S1/2 function Bit Clock. Used for general purpose read and write on the I2S bus.
-            Configuration.SetPinFunction(Gpio.IO33, DeviceFunction.I2S1_BCK);
-
-            //     I2S1/2 function WS. Used if your have stereo.
-            Configuration.SetPinFunction(Gpio.IO34, DeviceFunction.I2S1_WS);
-
-            //     I2S1/2 function DATA_OUT. Used for output data typically on a speaker.
-            Configuration.SetPinFunction(Gpio.IO32, DeviceFunction.I2S1_DATA_OUT);
-
-            //     I2S1/2 function MDATA_IN. Used for input data typically from a microphone.
-            //Configuration.SetPinFunction(Gpio.IO33, DeviceFunction.I2S1_MDATA_IN);
-
-
-
             // Init the ADC
             AdcController adc = new AdcController();
 
